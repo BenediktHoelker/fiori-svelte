51d1e0d1cb702ccbb608815a4f389bfa50f871ea
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2023-04-04 14:59:28 +0200

    chore: master/detail layout

--- src/routes/fcl/+page.svelte
+++ src/routes/fcl/+page.svelte
@@ -14,11 +14,16 @@
 	export let data;
 </script>
 
-<ui5-flexible-column-layout id="fcl" layout="TwoColumnsMidExpanded">
+<ui5-flexible-column-layout
+	id="fcl"
+	layout="TwoColumnsMidExpanded"
+	hide-arrows="true"
+	style="height: 100vh"
+>
 	<!-- start column -->
 	<div slot="startColumn">
 		<ui5-daterange-picker />
-		<ui5-list id="col1list" header-text="Employees" growing="Scroll" style="height: 100%">
+		<ui5-list id="col1list" header-text="Employees" growing="Scroll">
 			{#each data.workitems as workitem (workitem.title)}
 				<ui5-li>{workitem.title}</ui5-li>
 			{/each}
@@ -28,7 +33,7 @@
 	<!-- middle column -->
 	<ui5-page id="detailPage" background-design="List" slot="midColumn" show-footer>
 		<div slot="footer">
-			<ui5-bar design="FloatingFooter">
+			<ui5-bar>
 				<ui5-button design="Positive" slot="endContent">Agree</ui5-button>
 				<ui5-button design="Negative" slot="endContent">Decline</ui5-button>
 				<ui5-button design="Transparent" slot="endContent">Cancel</ui5-button>
